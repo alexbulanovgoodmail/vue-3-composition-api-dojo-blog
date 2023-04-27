f306a78405b08b8f7c35c9a76b7ae3ca6f2ad0f3
Alexander Bulanov <alex-bulanov.mail@yandex.ru>
2023-04-27 22:13:24 +0300

    Template Refs in the Setup Function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,15 +4,28 @@ import { onMounted, ref } from 'vue'
 console.log('[setup]')
 
 const caption = ref('Home View')
+const pRef = ref(null)
 
 //
 onMounted(() => {
   console.log('[onMounted]')
+  pRef.value.style = 'user-select: none;'
 })
+
+function handleClick(params) {
+  pRef.value.classList.toggle('active')
+}
 </script>
 
 <template>
   <main>
     <h1>{{ caption }}</h1>
+    <p ref="pRef" @click="handleClick">Lorem, ipsum dolor.</p>
   </main>
 </template>
+
+<style lang="scss">
+.active {
+  color: #ff0000;
+}
+</style>
