fb456b3aaf50af476e26c4d4dedb7fc554ed47cc
Alexander Bulanov <alex-bulanov.mail@yandex.ru>
2023-04-29 12:57:27 +0300

    Using watch and watchEffect

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,56 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, computed, ref, watch, watchEffect, handleError } from 'vue'
 
 console.log('[setup]')
 
 const caption = ref('Home View')
-const pRef = ref(null)
+const search = ref('')
+const names = ref(['mario', 'luigi', 'toad', 'yoshi', 'koopa', 'peach', 'bowser'])
+
+const matchingNames = computed(() => {
+  return names.value.filter((name) => name.includes(search.value))
+})
+
+const stopWatch = watch(search, () => {
+  console.log('[watch function]')
+})
+const stopEffect = watchEffect(() => {
+  console.log('[watchEffect]', search.value)
+})
 
 //
 onMounted(() => {
   console.log('[onMounted]')
-  pRef.value.style = 'user-select: none;'
 })
-
-function handleClick(params) {
-  pRef.value.classList.toggle('active')
+//
+function handleStopWatching(params) {
+  stopWatch()
+  stopEffect()
 }
 </script>
 
 <template>
   <main>
-    <h1>{{ caption }}</h1>
-    <p ref="pRef" @click="handleClick">Lorem, ipsum dolor.</p>
+    <div class="container">
+      <h1>{{ caption }}</h1>
+
+      <div class="my-6">
+        <div class="nes-field">
+          <label for="name_field">name</label>
+          <input v-model="search" type="text" id="name_field" class="nes-input" />
+        </div>
+      </div>
+      <div class="my-6">
+        <p v-for="(name, index) in matchingNames" :key="index">
+          {{ name }}
+        </p>
+      </div>
+      <div class="my-6">
+        <button class="nes-btn is-warning" type="button" @click="handleStopWatching">
+          Stop Watching
+        </button>
+      </div>
+    </div>
   </main>
 </template>
 
