86730ee8cd0d734fb44058910b7d9708a60e2995
Alexander Bulanov <alex-bulanov.mail@yandex.ru>
2023-04-30 21:01:06 +0300

    Connecting to a Firebase Project

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAeJh6MSVmpQA-cctpYHVaijsil2SCRTBY',
+  authDomain: 'udemy-vue-firebase-sites-daf09.firebaseapp.com',
+  projectId: 'udemy-vue-firebase-sites-daf09',
+  storageBucket: 'udemy-vue-firebase-sites-daf09.appspot.com',
+  messagingSenderId: '466046711902',
+  appId: '1:466046711902:web:d414ec63f208c865415ccf'
+}
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init firestore service
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
